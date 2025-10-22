684453321482ca83ce2652c8ea49fb86b3046791
Suchith <marupakasuchith@gmail.com>
2025-10-23 03:45:49 +0530

    Added Cloudinary Backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import multer from 'multer';
+import {v2 as cloudinary} from 'cloudinary';
 
 dotenv.config();
 
@@ -15,7 +17,7 @@ if (!URI) {
 
 mongoose.connect(URI)
 .then(()=>{console.log("MongoDb Connected")})
-.catch(()=>console.log("MongoDb Error: Could not Connect"));
+.catch((err)=>console.log("MongoDb Error: Could not Connect"));
 
 const mealSchema = new mongoose.Schema({
     // user_id:
@@ -25,14 +27,22 @@ const mealSchema = new mongoose.Schema({
 
 });
 
-const Meal = mongoose.model(mealSchema);
+const Meal = mongoose.model("Meal",mealSchema);
 
 app.use(express.json());
+
+const upload = multer({ dest: "uploads/" }); // temporary upload folder
+
+cloudinary.config({
+  cloud_name: process.env.cloud_name,
+  api_key: process.env.api_key,
+  api_secret: process.env.api_secret,
+});
 
 app.get('/meals',async(req,res) =>{
     try{
         const meals = await Meal.find();
-        res.json(meal);
+        res.json(meals);
     }
     catch(err){
         res.status(500).json({message: "Error Fetching Meals",error:err});
@@ -40,19 +50,32 @@ app.get('/meals',async(req,res) =>{
     
 }); //for History and photos.
 
-app.post("/meals", async(req,res) =>{
-    const {user_id, image_url, date, calories} = req.body;
-    if(!user_id || !image_url || !date || !calories){
+
+app.post("/meals", upload.single("image"),async(req,res) =>{
+    const {user_id, calories} = req.body;
+    if(!user_id || !calories){
         return res.status(400).json({message:"All fields are Mandatory"});
     }
     try{
+        
+        const result = await cloudinary.uploader.upload(req.file.path, {
+            folder: "calorie_app", // optional: to organize uploads
+        });
+
+        const image_url = result.secure_url;
+        const date = new Date();
+        if(!image_url){
+            return res.status(400).json({message: "Cloudinary Upload Failed"});
+        }
+
+
         const newMeal = new Meal({user_id,
             image_url,
             date,
             calories
         });
         await newMeal.save();
-        res.status(201);
+        res.status(201).json({message:"Added Successfully"});
     }
     catch(err){
         res.status(500).json({message:"Error Incurred",error:err});
